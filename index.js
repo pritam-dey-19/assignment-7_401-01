3ba705527bbd8a18611b0a419575ee504f22beb3
pritam-dey-19 <dey.pritam19@gmail.com>
2023-03-27 22:23:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+function myFunction() {
+  let text = document.getElementById("myInput").value;
+  document.getElementById("wrote").innerHTML = text; // read the value that written in the input box and show it in our p tag.
+}
